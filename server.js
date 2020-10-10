9673ef75624a6c8af94ad8a67b24308073734733
Michael Law <michaellaw@gmx.com>
2020-10-10 14:07:35 -0400

    setup assets with scss

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express  = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
+const sassMiddleware = require('node-sass-middleware')
 const host = process.env.HOST || '127.0.0.1'
 const app = express()
 const PORT = 11200
@@ -17,10 +18,24 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json())
 app.set('views', './views')
 app.set('view engine', 'liquid')
+app.use(sassMiddleware({
+  src: './assets/stylesheets',
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  force: true,
+  outputStyle: 'expanded'
+}))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.render('home.liquid')
 })
+
+app.get('/profile', (req, res) => {
+  res.render('profile.liquid')
+})
+
 
 app.listen(PORT, host, () => {
   console.log('running server')
